8b2cbc2ab7c046bfe9ff7441d8c9f36b2e22d62d
Valeriy <masterbox06@gmail.com>
2023-09-08 21:47:02 +0300

    Word Finder - solution with filter

--- Codewars/5/Word_Finder/index.js
+++ Codewars/5/Word_Finder/index.js
@@ -2,13 +2,18 @@ function Dictionary(words) {
   this.words = words;
 }
 
+// Dictionary.prototype.getMatchingWords = function (pattern) {
+//   const result = []
+//   pattern = new RegExp(`^${pattern.replace(/\?/g, ".")}$`)
+//   for (const word of this.words) {
+//     if (pattern.test(word)) result.push(word)
+//   }
+//   return result
+// }
+
 Dictionary.prototype.getMatchingWords = function (pattern) {
-  const result = []
   pattern = new RegExp(`^${pattern.replace(/\?/g, ".")}$`)
-  for (const word of this.words) {
-    if (pattern.test(word)) result.push(word)
-  }
-  return result
+  return this.words.filter(w => pattern.test(w))
 }
 
 
